e3d06a1d61eddaffbbed19961d8eafe90236cf8f
NguyenTuanMinh-user <ngtminh060206@gmail.com>
2026-09-22 22:18:42 +0700

    Create ChoosingOption_Lab01_Ex61.java

---
+++ ChoosingOption_Lab01_Ex61.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+public class ChoosingOption_Lab01_Ex61 {
+    public static void main(){
+        int option = JOptionPane.showConfirmDialog(null,
+                "Do you want to change the first class ticket");
+        JOptionPane.showMessageDialog(null,
+                "You have chosen: " + (option==JOptionPane.YES_OPTION?"Yes":"No"));
+        System.exit(0);
+    }
+}
